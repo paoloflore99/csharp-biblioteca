c2aaa08d8256193a416405bb5443503866a3b703
paolo flore <paoloflore937@gmail.com>
2024-04-22 15:23:17 +0200

    aggiunte classi per lutente

---
+++ csharp-biblioteca/Utente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Utente
+    {
+        public string Cognome { get; set; }
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public int Telefono { get; set; }
+
+
+
+    }
+}
---
+++ csharp-biblioteca/biblioteca.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class biblioteca
+    {
+
+
+
+    }
+}
+
+//Gli utenti si possono registrare al sistema, fornendo:
+//-cognome
+//- nome
+//- email
+//- password
+//- recapito telefonico
